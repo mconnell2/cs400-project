a253f19867247b03b68e13172041d05ed5632cea
mconnell2 <43834160+mconnell2@users.noreply.github.com>
2018-12-05 21:08:27 -0600

    updates following meeting on weds after class
    This should have sufficient methods for creating a Meal object in Main now

--- Meal.java
+++ Meal.java
@@ -3,21 +3,30 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /*
  * This class stores and evaluates meals based on one or more food items.
  */
 public class Meal {
   
-  List<FoodItem> foodItemList; //stores food items that comprise the meal
+  ObservableList<FoodItem> foodItemList; //stores food items that comprise the meal
 
   /*
    * constructor, empty meal
    */
   public Meal() {
-    foodItemList = new ArrayList<FoodItem>();
+    foodItemList = FXCollections.observableArrayList();
   }
   
+  /*
+   * retrieves meal
+   */
+  public ObservableList<FoodItem> getMeal() {
+    return foodItemList;
+  }
+    
   /*
    * adds a food item to the meal.
    * Duplicate food items may be added
@@ -27,15 +36,12 @@ public class Meal {
   }
   
   /*
-   * removes a food item from the meal.
+   * removes a food item from the meal from the specific index.
    * If the food item to remove does not exist, the meal is not changed.
    */
-  public void removeFoodItem(FoodItem foodItem) {
-
-    //iterate backwards through list; compare IDs
-    for (int i = foodItemList.size()-1; i >= 0; i--) {
-      if (foodItemList.get(i).getID().equals(foodItem.getID())) foodItemList.remove(i);
-    }
+  public void removeFoodItem(int index) {
+    if ((index >= 0) && index < foodItemList.size()) foodItemList.remove(index);
+  //TODO - show we throw an exception instead? We could a do try/catch around .remove method
   }
   
   /*
@@ -46,35 +52,34 @@ public class Meal {
   }
   
   /*
-   * Returns a string summarizing the nutritional information of all food items in the meal.
-   * 
-   * TODO - above is the class diagram info; I think we should return a hashmap
-   * as that will help with displaying later.
-   * TODO - we could get rid of this tag and instead update totals on every remove/add of an ingredient
+   * Returns a HashMap of nutrients (key, String) and grams (value, Double) for all food items
+   * in the meal
+   */
+  /**
+   * analyzes nutritional content of food items in the meal
+   * @return HashMap of nutrients (key, String) and grams (value, Double)
    */
   public HashMap<String, Double> analyzeMeal() {
     
-    //intialize HashMap for meal
+    //initialize HashMap for meal
     HashMap<String, Double> mealNutrients = new HashMap<String, Double>();
     
     //for each food item in the list
     for (FoodItem foodItem: foodItemList) {
       
       //retrieve nutrients
-      HashMap<String, Double> nutrients = foodItem.getNutrients();
+      HashMap<String, Double> foodNutrients = foodItem.getNutrients();
       
-      //for each nutrient, add to meal nutrients
-      for (String nutrient: nutrients.keySet()) {
+      //for each food item nutrient, add to meal nutrients
+      for (String nutrient: foodNutrients.keySet()) {
         Double currentMealTotal = mealNutrients.get(nutrient);
-        if (currentMealTotal == null) mealNutrients.put(nutrient, nutrients.get(nutrient));
-        else mealNutrients.put(nutrient, currentMealTotal + nutrients.get(nutrient));
+        if (currentMealTotal == null) mealNutrients.put(nutrient, foodNutrients.get(nutrient));
+        else mealNutrients.put(nutrient, currentMealTotal + foodNutrients.get(nutrient));
       }
     }
     
     return mealNutrients;
-    
   }
-  
   
   //TODO - not sure we need this for anything? It's not in our spec.
   //I also needed to add a toString method in foodItem.
@@ -85,42 +90,39 @@ public class Meal {
   
   //TODO remove main method after done with testing
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
     
+    //create food items
     FoodItem fi1 = new FoodItem("p1", "pasta");
     FoodItem fi2 = new FoodItem("p2", "pizza");
     FoodItem fi3 = new FoodItem("p3", "pickles");
     FoodItem fi4 = new FoodItem("p4", "pizza");
-   
     
+    //test constructor
     Meal meal = new Meal();
+    
+    //test adding food items, including duplicates
     meal.addFoodItem(fi1);
     meal.addFoodItem(fi2);
     meal.addFoodItem(fi3);
     meal.addFoodItem(fi4);
     meal.addFoodItem(fi4); //test adding duplicate
-    fi1.addNutrient("fat", 0.1);
-    fi2.addNutrient("fat", 0.1);
+    fi1.addNutrient("fat", 0.1); fi1.addNutrient("carbs", 0.1);
     fi3.addNutrient("fat", 1003.1);
-    fi2.addNutrient("fat", 10.0);
-    fi2.addNutrient("fat", 12.0);
-    fi2.addNutrient("carbs", 16.0);
+    fi2.addNutrient("fat", 10.0); fi2.addNutrient("fat", 12.0); fi2.addNutrient("carbs", 16.0);
     fi4.addNutrient("zinc", 22.1);
     
     //test custom toString method to see if everything was added
     System.out.println(meal.toString());
     
     //test clearing menu
-    /*
-    meal.clearMeal();
-    System.out.println(meal.toString());
-    */
+//    meal.clearMeal();
+//    System.out.println(meal.toString());
+    
     
     //test removing food item, including dups
-    /*
-    meal.removeFoodItem(fi4);
+    
+    meal.removeFoodItem(0);
     System.out.println(meal.toString());
-    */
     
     //analyzing meal for totals
     HashMap<String, Double> mealNutrients = meal.analyzeMeal();
